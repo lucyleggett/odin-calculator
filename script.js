2a3e5de4e9b9f8bc76ddaa8280d3ca9e45fa8df3
lucyleggett <lucy.leggett@hotmail.com>
2026-05-03 16:25:35 +0800

    Update sum function and create subtract function Functions now use reduce to allow for an undetermined number of arguments

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, ...n2) {
+    let total = +n1 + +n2;
 }
 
+function subtract(n1, ...n2) {
+    return n1 - n2;
+}
+
+// function multiply(n1, ...n2)
