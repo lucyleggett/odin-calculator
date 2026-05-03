422537cdc1cb2665a0d92e5c4419b89813c41e95
lucyleggett <lucy.leggett@hotmail.com>
2026-05-03 17:41:24 +0800

    Add buttons with classes and an event listener Button classes separated into number and operator to aid the operate function.

--- script.js
+++ script.js
@@ -27,9 +27,25 @@ function operate(num1, operator, num2) {
         add(num1, num2);
     } else if (operator === "-") {
         subtract(num1, num2);
-    } else if (operator === "*") {
+    } else if (operator === "×") {
         multiply(num1, num2);
-    } else if (operator === "/") {
+    } else if (operator === "÷") {
         divide(num1,num2);
     }
 }
+let clickCount = 0;
+
+const buttons = document.querySelector("button")
+buttons.addEventListener("click",(event) => {
+    clickCount++
+
+    if (clickCount === 1 && event.target.class === "number") {
+        num1 = event.target;
+    } else if (clickCount === 2 && event.target.class === "operator") {
+        operator = event.target;
+    } else if (clickCount === 3 && event.target.class === "number") {
+        num2 = event.target;
+    }
+})
+
+function evaluate
