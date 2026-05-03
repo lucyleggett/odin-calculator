be4c8ebcd940b610ee91f76c7a56ecc2206e2127
lucyleggett <lucy.leggett@hotmail.com>
2026-05-03 16:32:14 +0800

    Create multiply and divide functions and cater for user inputting zero arguments

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function add(n1, ...n2) {
-    let total = +n1 + +n2;
+function add(...numbers) {
+    if (numbers.length === 0) return 0;
+    return numbers.reduce((acc, curr) => acc + curr, 0);
 }
 
-function subtract(n1, ...n2) {
-    return n1 - n2;
+function subtract(...numbers) {
+    if (numbers.length === 0) return 0;
+    return numbers.reduce((acc, curr) => acc - curr, 0);
 }
 
-// function multiply(n1, ...n2)
+function multiply(...numbers) {
+    if (numbers.length === 0) return 0;
+    return numbers.reduce((acc, curr) => acc * curr, 1);
+}
+
+function divide(...numbers) {
+    if (numbers.length === 0) return 0;
+    return numbers.reduce((acc, curr) => acc / curr);
+}
