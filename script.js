1178e2423747453e7d09bfbeab036b909adc3ca9
lucyleggett <lucy.leggett@hotmail.com>
2026-05-05 12:20:38 +0800

    Fix subtract function and rewrite handleEvent logic Subtract function was returning -41 when the input was (35, 6). 0 index removed to fix the problem. HandleEvent now has two states only (initial and operate) and the display is updated within it.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function add(...numbers) {
 
 function subtract(...numbers) {
     if (numbers.length === 0) return 0;
-    return numbers.reduce((acc, curr) => acc - curr, 0);
+    return numbers.reduce((acc, curr) => acc - curr);
 }
 
 function multiply(...numbers) {
@@ -27,76 +27,89 @@ let result;
 
 function operate(num1, num2, operator) {
     let output;
-    if (operator === "add"){
-        output = add(num1, num2);
-    } else if (operator === "subtract") {
-        output = subtract(num1, num2);
-    } else if (operator === "multiply") {
-        output = multiply(num1, num2);
-    } else if (operator === "divide") {
-        output = divide(num1, num2);
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    if (operator === "+"){
+        return output = add(num1, num2);
+    } else if (operator === "-") {
+        return output = subtract(num1, num2);
+    } else if (operator === "×") {
+        return output = multiply(num1, num2);
+    } else if (operator === "÷") {
+        return output = divide(num1, num2);
     }
-    resetState(output);
-    result = output;
-}
-
-function resetState(output) {
-    state = "initial";
-    num1 = output;
-    num2 = "";
 }
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
     button.addEventListener("click", (event) => {
         handleEvent(event);
-        updateDisplay(event);
     });
 });
 
 let display = document.getElementById("display")
 
-function updateDisplay(event) {
-    if (state != "evaluate") {
-        display.textContent += event.target.innerText;
-    }
-    else if (state === "evaluate") {
-        display.textContent = result;
-}}
-
-function handleEvent(event) {
-    if (state === "initial") {
-        if (event.target.classList.contains("number")) {
+function handleEvent(event){
+    if (state === "initial"){
+        if (event.target.classList.contains("number")){
             num1 += event.target.innerText;
-            num1 = parseFloat(num1);
-        }
-        else if (event.target.classList.contains("operator")) {
-        state = "operate";
-        operator = event.target.id;
-        }
-    }
-    else if (state === "operate") {
-        if (event.target.classList.contains("number")) {
-            num2 += event.target.innerText;
-            num2 = parseFloat(num2);
-        }
-        else if (event.target.classList.contains("operator")) {
-            state = "evaluate";
-            result = operate(num1, num2, operator);
-            operator = event.target.id;
-        }
-        else if (event.target.classList.contains("evaluate")) {
-            state = "evaluate";
-            result = operate(num1, num2, operator);
-        }
-    }
-    else if (event.target.classList.contains("evaluate")) {
-        state = "evaluate";
-        operate(num1, num2, operator);
-    }
-    else if (state === "evaluate"){
-        if (event.target.classList.contains("number")) {
+            display.textContent += event.target.innerText;
+        } else if (event.target.classList.contains("operator")){
             state = "operate";
+            operator = event.target.innerText;
+            display.textContent += event.target.innerText;
+        }
+    }
+    else if (state === "operate"){
+        if (event.target.classList.contains("number")){
+            num2 += event.target.innerText;
+            display.textContent += event.target.innerText;
+        } else if (event.target.classList.contains("operator")){
+            result = operate(num1, num2, operator);
+            operator = event.target.innerText;
+            display.textContent = result + operator;
+            num1 = result;
+            num2 = "";
+        } else if (event.target.classList.contains("evaluate")){
+            result = operate(num1, num2, operator);
+            display.textContent = result;
         }
     }
 }
+
+// function handleEvent(event) {
+//     if (state === "initial") {
+//         if (event.target.classList.contains("number")) {
+//             num1 += event.target.innerText;
+//             num1 = parseFloat(num1);
+//         }
+//         else if (event.target.classList.contains("operator")) {
+//         state = "operate";
+//         operator = event.target.id;
+//         }
+//     }
+//     else if (state === "operate") {
+//         if (event.target.classList.contains("number")) {
+//             num2 += event.target.innerText;
+//             num2 = parseFloat(num2);
+//         }
+//         else if (event.target.classList.contains("operator")) {
+//             state = "evaluate";
+//             result = operate(num1, num2, operator);
+//             operator = event.target.id;
+//         }
+//         else if (event.target.classList.contains("evaluate")) {
+//             state = "evaluate";
+//             result = operate(num1, num2, operator);
+//         }
+//     }
+//     else if (event.target.classList.contains("evaluate")) {
+//         state = "evaluate";
+//         operate(num1, num2, operator);
+//     }
+//     else if (state === "evaluate"){
+//         if (event.target.classList.contains("number")) {
+//             state = "operate";
+//         }
+//     }
+// }
